65158f9f2d82a3e3550aa5cb0d943c5a4e82c772
Adedamola Emmanuel <mgcrons@gmail.com>
2022-11-22 08:23:25 +0100

    Added simple test

--- website/tests.py
+++ website/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+    
+class TestPages(TestCase):
+    
+    def test_home_page(self):
+        response =self.client.get('/', follow=True)
+        self.assertTemplateUsed(response, 'home.html')
+    
+    def test_about_page(self):
+        response = self.client.get('/about', follow=True)
+        self.assertTemplateUsed(response, 'about.html')
+    
+    def test_services_page(self):
+        response =self.client.get('/services', follow=True)
+        self.assertTemplateUsed(response, 'services.html')
